543f2fad1228337eee2f4b1489b0e2aefab73343
4UR3L10 <aureliomartinezdros@outlook.com>
2019-02-12 09:47:20 -0500

    Add files via upload

---
+++ guiAplicationTest/src/guiaplicationtest/GuiAplicationTest.java
@@ -0,0 +1,16 @@
+package guiaplicationtest;
+
+/**
+ *
+ * @author Aurelio Martinez Copyright. 2018
+ */
+public class GuiAplicationTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
